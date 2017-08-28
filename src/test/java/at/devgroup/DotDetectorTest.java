ba9ad96729c359f9ce1ccb86edeffd4f47f71fdf
Schuster <helmut.schuster@sozialbau.at>
2017-08-28 16:31:48 +0200

    Just adding a 2nd image...

--- src/test/java/at/devgroup/DotDetectorTest.java
+++ src/test/java/at/devgroup/DotDetectorTest.java
@@ -10,10 +10,12 @@ public class DotDetectorTest {
 
     //------------------------------------------------------------------------------------------------------------------
     @Test
-    public void testDoDetect() throws Exception {
+    public void testDoDetect01() throws Exception {
 
         DotDetector dotDetector = new DotDetector();
-        dotDetector.detectAndSaveInFile("src/test/resources/ImageTest.png", "target/ImageTest_GreyScale.png");
+
+        dotDetector.detectAndSaveInFile("src/test/resources/ImageTest01.jpg", "target/ImageTest01_Detected.png");
+        dotDetector.detectAndSaveInFile("src/test/resources/ImageTest02.jpg", "target/ImageTest02_Detected.png");
     }
 
     //------------------------------------------------------------------------------------------------------------------
